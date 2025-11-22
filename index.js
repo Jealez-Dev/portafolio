394a6ba22e68beb9c93cbc302d5a2e6eff8fa8fe
Jeanniel <jeanniel.zelaznog@gmail.com>
2025-11-22 11:55:20 -0400

    preparado para vercel

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-import app from './server/server.js'
+import { Elysia } from 'elysia'
+import { staticPlugin } from '@elysiajs/static'
+import { prueba } from './server/api/prueba.js'
+import { proyectos } from './server/api/supabase.js'
+
+const app = new Elysia()
+    .use(staticPlugin({
+        assets: 'public',
+        prefix: '/'
+    }))
+    .get('/', () => Bun.file(`${import.meta.dir}/public/index.html`))
+    .use(prueba)
+    .use(proyectos)
 
 export default app
+
+if (import.meta.main) {
+    app.listen(3000)
+    console.log('🦊 Servidor corriendo en http://localhost:3000')
+}
